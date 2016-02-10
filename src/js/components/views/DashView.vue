a0c7ae666d3a6e8cb36949f9b2c8bbefcb3b4029
SimulatedGREG <simulatedgreg@gmail.com>
2016-02-09 19:56:08 -0600

    breakdown dashview

--- src/js/components/views/DashView.vue
+++ src/js/components/views/DashView.vue
@@ -14,30 +14,7 @@
   <view-header title="Dash"></view-header>
 
   <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--8-col">
-        <fieldset>
-
-          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" v-bind:class="{ 'is-dirty': Twitch.channel.status }">
-            <input class="mdl-textfield__input" type="text" id="stream-title" v-model="Twitch.channel.status">
-            <label class="mdl-textfield__label" for="stream-title">Title</label>
-          </div>
-
-          <games-typeahead></games-typeahead>
-
-          <p class="mdl-slider-wrapper">
-            <label for="stream-delay">
-              Delay
-              <span>{{ Twitch.channel.delay }} seconds</span>
-            </label>
-            <input class="mdl-slider mdl-js-slider" id="stream-delay" type="range" min="0" max="60" v-model="Twitch.channel.delay">
-          </p>
-
-          <button class="mdl-button mdl-js-button mdl-button--raised" @click="putChannel">
-            Update
-          </button>
-
-      </fieldset>
-    </div>
+    <stream-editor></stream-editor>
 
     <div class="mdl-cell mdl-cell--4-col">
       <!-- <iframe src="http://www.twitch.tv/simulatedgreg/chat?popout=" frameborder="0" scrolling="no"></iframe> -->
@@ -48,7 +25,7 @@
 <script>
   import Twitch from '../../vendor/twitch';
   import ViewHeader from '../ViewHeader.vue';
-  import GamesTypeahead from './DashView/GamesTypeahead.vue';
+  import StreamEditor from './DashView/StreamEditor.vue';
 
   export default {
     data() {
@@ -78,7 +55,7 @@
     },
     components: {
       ViewHeader,
-      GamesTypeahead
+      StreamEditor
     }
   }
 </script>
---
+++ src/js/components/views/DashView/StreamEditor.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="mdl-cell mdl-cell--8-col">
+    <fieldset>
+
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" v-bind:class="{ 'is-dirty': Twitch.channel.status }">
+        <input class="mdl-textfield__input" type="text" id="stream-title" v-model="Twitch.channel.status">
+        <label class="mdl-textfield__label" for="stream-title">Title</label>
+      </div>
+
+      <games-typeahead></games-typeahead>
+
+      <p class="mdl-slider-wrapper">
+        <label for="stream-delay">
+          Delay
+          <span>{{ Twitch.channel.delay }} seconds</span>
+        </label>
+        <input class="mdl-slider mdl-js-slider" id="stream-delay" type="range" min="0" max="60" v-model="Twitch.channel.delay">
+      </p>
+
+      <button class="mdl-button mdl-js-button mdl-button--raised" @click="putChannel">
+        Update
+      </button>
+
+    </fieldset>
+  </div>
+</template>
+
+<script>
+  import Twitch from '../../../vendor/twitch';
+  import Typeahead from 'vue-typeahead';
+  import GamesTypeahead from './StreamEditor/GamesTypeahead.vue';
+
+  export default {
+    components: { GamesTypeahead },
+    data() {
+      return {
+        Twitch: this.$root.Twitch
+      }
+    },
+    methods: {
+      putChannel() {
+        Twitch.api({
+          method: 'channels/' + this.Twitch.channel.name,
+          params: {
+            channel: {
+              status: this.Twitch.channel.status,
+              game: this.Twitch.channel.game,
+              delay: this.Twitch.channel.delay
+            }
+          },
+          verb: 'PUT'
+        }, () => { this.$root.auth(true); });
+      }
+    },
+    events: {
+      'auth': function() {
+        this.Twitch = this.$root.Twitch;
+      }
+    }
+  }
+</script>
